e35882f22acfcf2dadd0e9dc6c3f8ef102567980
Jay Ramos <89936073+ramosJay@users.noreply.github.com>
2021-12-14 21:49:17 -1000

    Landing page update

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -28,6 +28,52 @@ class Landing extends React.Component {
             </Grid.Column>
           </Grid>
         </div>
+        <div className='landing-white-background'>
+          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>First, Register your account by either signing up as a Student or a Company.</Header>
+          <Grid container stackable columns='equal' textAlign='center'>
+            <Grid.Column>
+              <Image src="/images/sign-upPageS.png"/>
+              <Image src="/images/sign-upPageC.png"/>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className='landing-white-background'>
+          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>As a Student, you can create and edit your profile from the student homepage. You will then be directed to the edit page, where you can store your information. </Header>
+          <Grid container stackable columns='equal' textAlign='center'>
+            <Grid.Column>
+              <Image src="/images/studentLand.png"/>
+              <Image src="/images/addStudentProfile.png"/>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className='landing-white-background'>
+          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>As a Company, you can also create and edit your profile from the Company homepage. You will then be directed to the edit page, where you can store your information. </Header>
+          <Grid container stackable columns='equal' textAlign='center'>
+            <Grid.Column>
+              <Image src="/images/compLand.png"/>
+              <Image src="/images/addComp.png"/>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className='landing-white-background'>
+          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>As a Student, you can find companies using the Find Companies button in the top menu. There, Students can filter their search results by their interests. </Header>
+          <Grid container stackable columns='equal' textAlign='center'>
+            <Grid.Column>
+              <Image src="/images/findComp.png"/>
+              <Image src="/images/findCompFilter.png"/>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className='landing-white-background'>
+          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>Similar, Companies have the ability to search for students using the
+            Find Students button in the top menu. There Companies can also filter students based on interests.</Header>
+          <Grid container stackable columns='equal' textAlign='center'>
+            <Grid.Column>
+              <Image src="/images/findStu.png"/>
+              <Image src="/images/findStuFilter.png"/>
+            </Grid.Column>
+          </Grid>
+        </div>
       </div>
     );
   }
